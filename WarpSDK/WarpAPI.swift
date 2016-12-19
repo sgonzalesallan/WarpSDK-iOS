cf097bbf3219c9e69b0112e03e2007388776ee1e
Zonily Jame Pesquera <zonilyjame@gmail.com>
2016-12-19 11:12:23 +0800

    Custom API Calls now return an Alamofire.Request object

--- WarpSDK/WarpAPI.swift
+++ WarpSDK/WarpAPI.swift
@@ -10,42 +10,42 @@ import Alamofire
 //import EVReflection
 
 public class WarpAPI {
-    public static func get(URLString: URLStringConvertible, parameters: [String : AnyObject]?, headers: [String : String], completionHandler: (warpResult:WarpResult) -> Void){
+    public static func get(URLString: URLStringConvertible, parameters: [String : AnyObject]?, headers: [String : String], completionHandler: (warpResult:WarpResult) -> Void) -> Request {
         if parameters == nil {
-            Alamofire.request(.GET, URLString, encoding: .URL, headers: headers)
+            return Alamofire.request(.GET, URLString, encoding: .URL, headers: headers)
                 .responseJSON { (response) in
                     completionHandler(warpResult: WarpTools.toResult(response))
             }
         } else {
-            Alamofire.request(.GET, URLString, parameters: parameters, encoding: .URL, headers: headers)
+            return Alamofire.request(.GET, URLString, parameters: parameters, encoding: .URL, headers: headers)
                 .responseJSON { (response) in
                     completionHandler(warpResult: WarpTools.toResult(response))
             }
         }
     }
     
-    public static func post(URLString: URLStringConvertible, parameters: [String : AnyObject]?, headers: [String : String], completionHandler: (warpResult:WarpResult) -> Void){
+    public static func post(URLString: URLStringConvertible, parameters: [String : AnyObject]?, headers: [String : String], completionHandler: (warpResult:WarpResult) -> Void) -> Request {
         if parameters == nil {
-            Alamofire.request(.POST, URLString, encoding: .JSON, headers: headers)
+            return Alamofire.request(.POST, URLString, encoding: .JSON, headers: headers)
                 .responseJSON { (response) in
                     completionHandler(warpResult: WarpTools.toResult(response))
             }
         } else {
-            Alamofire.request(.POST, URLString, parameters: parameters, encoding: .JSON, headers: headers)
+            return Alamofire.request(.POST, URLString, parameters: parameters, encoding: .JSON, headers: headers)
                 .responseJSON { (response) in
                     completionHandler(warpResult: WarpTools.toResult(response))
             }
         }
     }
     
-    public static func put(URLString: URLStringConvertible, parameters: [String : AnyObject]?, headers: [String : String], completionHandler: (warpResult:WarpResult) -> Void){
+    public static func put(URLString: URLStringConvertible, parameters: [String : AnyObject]?, headers: [String : String], completionHandler: (warpResult:WarpResult) -> Void) -> Request {
         if parameters == nil {
-            Alamofire.request(.PUT, URLString, encoding: .JSON, headers: headers)
+            return Alamofire.request(.PUT, URLString, encoding: .JSON, headers: headers)
                 .responseJSON { (response) in
                     completionHandler(warpResult: WarpTools.toResult(response))
             }
         } else {
-            Alamofire.request(.PUT, URLString, parameters: parameters, encoding: .JSON, headers: headers)
+            return Alamofire.request(.PUT, URLString, parameters: parameters, encoding: .JSON, headers: headers)
                 .responseJSON { (response) in
                     completionHandler(warpResult: WarpTools.toResult(response))
             }
@@ -53,14 +53,14 @@ public class WarpAPI {
         
     }
     
-    public static func delete(URLString: URLStringConvertible, parameters: [String : AnyObject]?, headers: [String : String], completionHandler: (warpResult:WarpResult) -> Void){
+    public static func delete(URLString: URLStringConvertible, parameters: [String : AnyObject]?, headers: [String : String], completionHandler: (warpResult:WarpResult) -> Void) -> Request {
         if parameters == nil {
-            Alamofire.request(.DELETE, URLString, encoding: .JSON, headers: headers)
+            return Alamofire.request(.DELETE, URLString, encoding: .JSON, headers: headers)
                 .responseJSON { (response) in
                     completionHandler(warpResult: WarpTools.toResult(response))
             }
         } else {
-            Alamofire.request(.DELETE, URLString, parameters: parameters, encoding: .JSON, headers: headers)
+            return Alamofire.request(.DELETE, URLString, parameters: parameters, encoding: .JSON, headers: headers)
                 .responseJSON { (response) in
                     completionHandler(warpResult: WarpTools.toResult(response))
             }
