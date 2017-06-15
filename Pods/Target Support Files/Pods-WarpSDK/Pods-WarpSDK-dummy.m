d7ccb33802e38f45e73a4097fd3c7d9618ea44f5
Zee <pesquera@mclinica.com>
2017-06-15 10:02:13 +0800

    Removed Pod files on git

--- Pods/Target Support Files/Pods-WarpSDK/Pods-WarpSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WarpSDK : NSObject
-@end
-@implementation PodsDummy_Pods_WarpSDK
-@end
--- Pods/Target Support Files/Pods-WarpSDKTests/Pods-WarpSDKTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WarpSDKTests : NSObject
-@end
-@implementation PodsDummy_Pods_WarpSDKTests
-@end
